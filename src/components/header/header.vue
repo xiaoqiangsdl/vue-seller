0f94941e8b1b0b8e003c37fbff4f2f0a89dfee40
evan <evan@codesword.local>
2016-12-19 21:58:34 +0800

    12-19 commit

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,9 +17,17 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div class="bulletin-wrapper">
-
+      <div class="support-count" v-if="seller.supports">
+        <span class="count">{{seller.supports.length}}</span>
+        <i class="icon-keyboard_arrow_right"></i>
       </div>
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
     </div>
   </div>
 </template>
@@ -41,9 +49,12 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin"
   .header
+    position: relative
     color: #fff
-    background: #000
+    background: rgba(7, 17, 27, .5)
+    overflow: hidden
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
       font-size: 0
       .avatar
@@ -95,5 +106,60 @@
           .text
             line-height: 12px
             font-size: 10px
+
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 18px
+        padding: 0 8px
+        height: 24px
+        color: #fff
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, .2)
+        text-align: center
+        .count
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          line-height: 24px
+          margin-left: 2px
+          font-size: 10px
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7,17,27,.2)
+      .bulletin-title
+        display: inline-block;
+        vertical-align: top
+        width: 22px
+        height: 12px
+        margin-top: 7px
+        bg-image('bulletin')
+        background-size: 22px 12px;
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        margin: 0 5px
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px;
+        right: 12px
+        top: 8px
+    .background
+      position: absolute
+      left: 0
+      top: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+
+
 
 </style>
